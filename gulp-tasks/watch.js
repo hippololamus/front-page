c2f04ed5d4dd9b8a45833fd8415e2b757e2a6677
Shane McDermed <shmcdermed@gmail.com>
2015-03-17 16:41:33 -0400

    add less files to the gulp watch task

--- gulp-tasks/watch.js
+++ gulp-tasks/watch.js
@@ -5,4 +5,5 @@ var config = require('../config');
 
 gulp.task('watch', function() {
     gulp.watch(config.buildDir.src + config.buildDir.scripts, ['scripts']);
+    gulp.watch(config.styles.src, ['styles']);
 });
